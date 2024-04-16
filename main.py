722fe8d8ea0723877cdf449615d390608d0cae15
Liam Doyle <l.doyler04@gmail.com>
2024-04-16 13:36:05 +0100

    Basic terrain added, no gravity or collision yet

--- main.py
+++ main.py
@@ -6,17 +6,66 @@ from pygame.locals import *
 
 pygame.init()
 
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 900
+# define screen information
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 450
 pygame.display.set_caption("My First Game!")
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# load images
+background_image = pygame.image.load("images/background.png")
+dirt_image = pygame.image.load("images/dirt_placeholder.png")
+
 fps = 60
 
+# define size variables
+tile_size = 50
+# player_size = (height, width)
+
+def draw_grid():
+	for line in range(0, 20):
+		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size))
+		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, SCREEN_HEIGHT))
+
+
 class World():
-    def __init__(self):
+    def __init__(self, data):
+        self.tile_list = []
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_image, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
         
+
+world_data = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+]
+
+world = World(world_data)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -52,9 +101,14 @@ player = Player()
 
 all_sprites = pygame.sprite.Group()
 
+
+# Game loop. The code from here on is mainly event handling.
 running = True
 
 while running:
+
+    screen.blit(pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT)), (0, 0))
+
     for event in pygame.event.get():
 
         if event.type == KEYDOWN:
@@ -68,7 +122,10 @@ while running:
 
     player.update(pressed_keys)
 
-    screen.fill((0, 0, 0))
     screen.blit(player.surf, player.rect)
+
+    world.draw()
+
+    draw_grid()
 
     pygame.display.flip()
